c440131641f7251e235a8813bce48253c8fef04f
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2021-02-07 13:05:34 +0100

    ✨ : Feat. New Service : Calendar Service

---
+++ packages/calendar-service/src/api/routes/v1/calendar.routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const controller = require('../../controllers/calendar.controller')
+
+const router = express.Router()
+
+// protected route
+router.route('/status').get(controller.getStatus)
+
+router.route('/:calendarId').get(controller.getAllCourse)
+
+router.route('/:calendarId/week/:nextWeek*?').get(controller.getWeekCourses)
+
+router.route('/:calendarId/day/:nextDay*?').get(controller.getDayCourses)
+
+router.route('/:calendarId/:idCours').get(controller.getCoursById)
+
+module.exports = router
---
+++ packages/calendar-service/src/constants/constants.prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  logs: 'production',
+  secretToken: process.env.secretToken
+}
