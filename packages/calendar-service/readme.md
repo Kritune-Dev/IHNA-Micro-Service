724bd3055b5b57c2c4de8d079af160bb00e9d43c
Kritune <corentin.bacconnais@gmail.com>
2021-02-07 13:19:34 +0100

    Passage TS --> JS (#2)
    * 🔨 : Move workflows.sh in script folder
    
    * 📝 : Add Global Readme.md
    
    * 🔥 🚧 : Restart base
    
    * ✨ : Feat. New Service : Calendar Service
    
    * 💡 : Add Badge Readme
    
    * 🔥 : Delete useless folder/file

---
+++ packages/calendar-service/readme.md
@@ -0,0 +1,14 @@
+## NodeJS microservice and deploy it to Docker
+
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+![CI/CD](https://github.com/Kritune-Dev/IHNA-Micro-Service/workflows/calendar-service-CI/CD/badge.svg)
+
+The **Cakendar service** microservice and API.
+
+### Stack
+- NodeJS 
+- Docker
+- GoogleApi
+- GoogleAuth
+
+[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
---
+++ packages/calendar-service/src/api/services/information.service.js
@@ -0,0 +1,50 @@
+const packageInformation = require('../../../package.json')
+
+exports.packageParseInformation = (req) => {
+  const uptime = getUptime()
+  const revision = getGitCommit()
+  return {
+    message: 'OK',
+    timestamp: new Date().toISOString(),
+    name: packageInformation.name,
+    version: packageInformation.version,
+    uptime: uptime,
+    revision: revision,
+    IP: req.ip,
+    URL: req.originalUrl
+  }
+}
+
+// eslint-disable-next-line no-extend-native
+String.prototype.toHHMMSS = function () {
+  const secNum = parseInt(this, 10) // don't forget the second param
+  let hours = Math.floor(secNum / 3600)
+  let minutes = Math.floor((secNum - hours * 3600) / 60)
+  let seconds = secNum - hours * 3600 - minutes * 60
+
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+  const time = hours + ':' + minutes + ':' + seconds
+  return time
+}
+
+function getUptime () {
+  const time = process.uptime()
+  const uptime = (time + '').toHHMMSS()
+  return uptime
+}
+
+function getGitCommit () {
+  const revision = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString()
+    .trim()
+  return revision
+}
